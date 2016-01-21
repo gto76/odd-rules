81228f17ba358304b63847ea41f5bb1d781c2460
Jure Sorn <sornjure@gmail.com>
2016-01-21 22:56:20 +0100

    General rules implemented

--- source/app.d
+++ source/app.d
@@ -1,161 +1,19 @@
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.csv;
+import std.file;
+import std.range;
+import std.regex;
 import std.stdio;
 import std.string;
-import std.array;
-import std.range;
-import std.conv;
-
-import std.csv;
-import std.algorithm;
 import std.typecons;
 
-enum Team { H, A }
-enum Res : Team { H = Team.H, D, A = Team.A}
-enum NumericOperator { lt = "<", mt = ">" }
-enum LogicOperator { AND, OR }
+import rule;
 
-//////////
-// RULE //
-//////////
-
-class Rule {
-  GeneralRule[] generalRules;
-  TeamRule[] teamRules;
-  LogicOperator[] logicOperators;
-  this(GeneralRule[] generalRules, TeamRule[] teamRules,
-       LogicOperator[] logicOperators) {
-    this.generalRules = generalRules;
-    this.teamRules = teamRules;
-    this.logicOperators = logicOperators;
-  }
-  /**
-  * ("country" = ["germany", "england"]) AND (A, "corners", 0) < 0.5 AND (H, "fouls", 1) > 0.5
-  */
-  override string toString() {
-    string[] ret;
-    foreach (val; generalRules) {
-      ret ~= to!string(LogicOperator.AND);
-      ret ~= val.toString();
-    }
-    foreach (val; zip(LogicOperator.AND ~ logicOperators, teamRules)) {
-      ret ~= to!string(val[0]);
-      ret ~= val[1].toString();
-    }
-    ret = ret[1..$];
-    return ret.join(" ");
-  }
-}
-
-//////////////////
-// GENERAL RULE //
-//////////////////
-
-abstract class GeneralRule {
-  string parameter;
-  this(string parameter) {
-    this.parameter = parameter;
-  }
-}
-
-// League level, ...
-class DigitRule : GeneralRule {
-  NumericOperator numericOperator;
-  double constant;
-  this(string parameter, NumericOperator numericOperator, double constant) {
-    super(parameter);
-    this.numericOperator = numericOperator;
-    this.constant = constant;
-  }
-  override string toString() {
-    return [parameter, numericOperator, to!string(constant)].join(" ");
-  }
-}
-
-// Country, sport, ...
-class DiscreteRule : GeneralRule {
-  string[] values;
-  this(string parameter,string[] values) {
-    super(parameter);
-    this.values = values;
-  }
-  override string toString() {
-    return parameter ~ " = (\"" ~ values.join("\", \"") ~ "\")";
-  }
-}
-
-///////////////
-// TEAM RULE //
-///////////////
-
-abstract class TeamRule {
-  Parameter parameter;
-  NumericOperator numericOperator;
-  this(Parameter parameter, NumericOperator numericOperator) {
-    this.parameter = parameter;
-    this.numericOperator = numericOperator;
-  }
-}
-
-class TeamRuleWithOtherParameter : TeamRule {
-  Parameter otherParameter;
-  this(Parameter parameter, NumericOperator numericOperator,
-       Parameter otherParameter) {
-    super(parameter, numericOperator);
-    this.otherParameter = otherParameter;
-  }
-  override string toString() {
-    return [parameter.toString(), numericOperator, otherParameter.toString()].join(" ");
-  }
-}
-
-class TeamRuleWithConstant : TeamRule {
-  double constant;
-  this(Parameter parameter, NumericOperator numericOperator,
-       double constant) {
-    super(parameter, numericOperator);
-    this.constant = constant;
-  }
-  override string toString() {
-    return [parameter.toString(), numericOperator, to!string(constant)].join(" ");
-  }
-}
-
-///////////////
-// PARAMETER //
-///////////////
-
-abstract class Parameter {
-  string name;
-  Team team;
-  this(string name, Team team) {
-    this.team = team;
-    this.name = name;
-  }
-}
-
-class ParameterWholeSeason : Parameter {
-  this(string name, Team team) {
-    super(name, team);
-  }
-  override string toString() {
-    return "(\"" ~ [name, to!string(team)].join("\", \"") ~ "\")";
-  }
-}
-
-class ParameterForLastGames : Parameter {
-  int numberOfGames;
-  this(string name, Team team, int numberOfGames) {
-    super(name, team);
-    this.numberOfGames = numberOfGames;
-  }
-  override string toString() {
-    //return "(" ~ [name, to!string(team), to!string(numberOfGames)].join(", ") ~ ")";
-    return  "(" ~ ["\""~name~"\"", to!string(team), to!string(numberOfGames)].join(", ") ~ ")";
-  }
-}
-
-//////////
-// MAIN //
-//////////
+public static const bool USE_AVERAGE_ODDS = true;
+public static const string BETBRAIN_AVERAGE = "BbAv";
+public static const string BETBRAIN_MAX = "BbMx";
 
 class ProfitAndOccurances {
   double[Res] profit;
@@ -165,88 +23,172 @@ class ProfitAndOccurances {
     profit[Res.D] = 0;
     profit[Res.A] = 0;
   }
+  public double getAvgProfit(Res res) {
+    return profit[res] / occurances;
+  }
+  override public string toString() {
+    return "H: " ~ to!string(getAvgProfit(Res.H)) ~ " D: " ~ to!string(getAvgProfit(Res.D))
+           ~ " A: " ~ to!string(getAvgProfit(Res.A)) ~ " occ: " ~ to!string(occurances);
+  }
 }
 
-class SeasonWithHeader {
+class Season {
+  string[string] features;
   string[] header;
   string[string][] games;
-  this(string[] header, string[string][] games) {
+  this(string[string] features, string[] header, string[string][] games) {
+    this.features = features;
     this.header = header;
     this.games = games;
   }
 }
 
+//////////
+// MAIN //
+//////////
+
 void main(string[] args) {
-  Rule rule = new Rule([new DiscreteRule("country", ["germany", "england"])],
+  // TODO season in for of: before current.
+  Rule rule = new Rule([new DiscreteRule("country", ["germany", "england"]), new DigitRule("season", NumericOperator.lt, 2014)],
                        [new TeamRuleWithConstant(new ParameterForLastGames("corners", Team.A,  0), NumericOperator.lt, 0.5),
-                       new TeamRuleWithConstant(new ParameterForLastGames("fouls", Team.H, 1), NumericOperator.mt, 0.5)],
+                        new TeamRuleWithConstant(new ParameterForLastGames("fouls", Team.H, 1), NumericOperator.mt, 0.5)],
                        [LogicOperator.AND]);
-  writeln(rule);
   //foreach(record; file.byLine.joiner("\n").csvReader!(Tuple!(string, string, string, string, int, int , string, int , int, string, string, int, int, int, int, int, int , int, int, int, int, int, int, double, double, int, double, double, double, double, double, double, double, int, double, double, int, double, double, double, double, double, double, double, int , double, double, double, double, double, int, double, double, double, double, double, double, int, double, double, double, double, int, double, double, double, double, double)))
-  auto file = File("E0.csv", "r");
-  auto records = csvReader!(string[string])(file.byLine.joiner("\n"), null);
-  string[string][] games;
-  foreach(record; records) {
-    games ~= record;
-  }
-  auto swh = new SeasonWithHeader(records.header, games);
-  file.close();
-  auto profitAndOccurances = getProfitAndOccurances([swh], rule);
+  Season[] seasons = loadSeasonsFromDir("csv");
+  writeln("Analizing rule: \n"~to!string(rule));
+  ProfitAndOccurances profitAndOccurances = getProfitAndOccurances(seasons, rule);
+  writeln(profitAndOccurances);
+  writeln("The End");
 }
 
-ProfitAndOccurances getProfitAndOccurances(SeasonWithHeader[] swhs, Rule rule) {
-//  writeln(swhs[0].games[0]["Div"]);
-//  writeln(swhs[0].header[4]);
+private Season[] loadSeasonsFromDir(string dir) {
+  Season[] seasons;
+  foreach(fileName; dirEntries(dir, "*.csv", SpanMode.shallow)) {
+    writeln("$$$ Loading season file: "~fileName);
+    auto file = File(fileName, "r");
+    auto records = csvReader!(string[string])(file.byLine.joiner("\n"), null);
+    string[string][] games;
+    foreach(record; records) {
+      games ~= record;
+    }
+    auto features = getSeasonsFeatures(fileName);
+    auto season = new Season(features, records.header, games);
+    seasons ~= season;
+    file.close();
+  }
+  return seasons;
+}
+
+private string[string] getSeasonsFeatures(string fileName) {
+  string[] header = ["sport", "country", "league", "season"];
+  int i = 0;
+  string[string] features;
+  string fileNameNoExtension = split(fileName, ".")[0];
+  fileNameNoExtension = split(fileNameNoExtension, "\\")[1];
+  foreach(feature; split(fileNameNoExtension, "-")) {
+    features[header[i++]] = to!string(feature);
+  }
+  return features;
+}
+
+private ProfitAndOccurances getProfitAndOccurances(Season[] seasons, Rule rule) {
   auto pao = new ProfitAndOccurances();
-  foreach (swh; swhs) {
-    foreach (game; swh.games) {
-      if (ruleAplies(game, rule)) {
+  foreach (season; seasons) {
+    if (!seasonFitsTheRule(season, rule)) {
+      continue;
+    }
+    foreach (game; season.games) {
+      if (ruleAplies(game, season.games, rule)) {
         pao.occurances++;
         Res res = getResult(game);
         double profit = getProfit(game);
-        if (res == Res.H) {
-          pao.profit[Res.H] += profit;
-          pao.profit[Res.D] -= 1;
-          pao.profit[Res.A] -= 1;
-        } else if (res == Res.D) {
-          pao.profit[Res.H] -= 1;
-          pao.profit[Res.D] += profit;
-          pao.profit[Res.A] -= 1;
-        } else {
-          pao.profit[Res.H] -= 1;
-          pao.profit[Res.D] -= 1;
-          pao.profit[Res.A] += profit;
-        }
+        setProfit(pao, res, profit);
       }
     }
   }
   return pao;
 }
 
-bool ruleAplies(string[string] game, Rule rule) {
+private bool seasonFitsTheRule(Season season, Rule rule) {
+  foreach (generalRule; rule.generalRules) {
+    if (!ruleApplies(generalRule, season.features[generalRule.parameter])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+private bool ruleApplies(GeneralRule rule, string parameterValue) {
+  if (typeid(rule) == typeid(DigitRule)) {
+    DigitRule digitRule = cast(DigitRule) rule;
+    if (digitRule.numericOperator == NumericOperator.lt) {
+      return to!double(parameterValue) < digitRule.constant;
+    } else {
+      return to!double(parameterValue) >= digitRule.constant;
+    }
+  } else if (typeid(rule) == typeid(DiscreteRule)) {
+    DiscreteRule disRule = cast(DiscreteRule) rule;
+    return disRule.values.canFind(parameterValue);
+  }
   return false;
 }
 
-Res getResult(string[string] game) {
-  return Res.H;
+private void setProfit(ProfitAndOccurances pao, Res res, double profit) {
+  if (res == Res.H) {
+    pao.profit[Res.H] += profit-1;
+    pao.profit[Res.D] -= 1;
+    pao.profit[Res.A] -= 1;
+  } else if (res == Res.D) {
+    pao.profit[Res.H] -= 1;
+    pao.profit[Res.D] += profit-1;
+    pao.profit[Res.A] -= 1;
+  } else if (res == Res.A) {
+    pao.profit[Res.H] -= 1;
+    pao.profit[Res.D] -= 1;
+    pao.profit[Res.A] += profit-1;
+  }
 }
 
-double getProfit(string[string] game) {
-  return 0;
+private bool ruleAplies(string[string] game, string[string][] games, Rule rule) {
+  bool result = false;
+  foreach (val; zip(LogicOperator.OR ~ rule.logicOperators, rule.teamRules)) {
+    auto operator = val[0];
+    auto teamRule = val[1];
+    bool teamRuleEval = evalTeamRule(teamRule, game, games);
+    if (operator == LogicOperator.OR) {
+      result = result || teamRuleEval;
+    } else if (operator == LogicOperator.AND) {
+      if (!teamRuleEval) {
+        return false;
+      }
+    }
+  }
+  return result;
 }
 
+private bool evalTeamRule(TeamRule teamRule, string[string] game, string[string][] games) {
+// TODO
+//  if (typeid(teamRule) == typeid(TeamRuleWithConstant)) {
+//  }
+  return true;
+}
 
+private Res getResult(string[string] game) {
+  string sResult = game["FTR"];
+  return to!Res(sResult);
+}
 
-
-
-
-
-
-
-
-
-
-
-
+private double getProfit(string[string] game) {
+  Res result = getResult(game);
+  string columnBase = "";
+  if (USE_AVERAGE_ODDS) {
+    columnBase = BETBRAIN_AVERAGE;
+  } else {
+    columnBase = BETBRAIN_MAX;
+  }
+  string column = columnBase ~ to!string(result);
+  string sProfit = game[column];
+  return to!double(sProfit);
+}
 
 
