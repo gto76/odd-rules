bdb663583a114a727d3dd34f5bb4d6c82fd78394
Jure Sorn <sornjure@gmail.com>
2016-01-26 20:44:45 +0100

    Added bash script that draws a plot of rules

--- results/display-graph.py
+++ results/display-graph.py
@@ -10,9 +10,11 @@ import csv
 import matplotlib.pyplot as plt
 
 def main():
-  reader = csv.reader(open('random-rules-dist', newline='\n'), delimiter=',', quotechar='"')
+  reader = csv.reader(open(sys.argv[1], newline='\n'), delimiter=',', quotechar='"')
   rdr = list(reader)
-  plt.plot(rdr)
+  column1 = [row[0] for row in rdr]
+  column2 = [row[1] for row in rdr]
+  plt.plot(column1, column2, 'ro')
   plt.show()
 
 if __name__ == '__main__':
